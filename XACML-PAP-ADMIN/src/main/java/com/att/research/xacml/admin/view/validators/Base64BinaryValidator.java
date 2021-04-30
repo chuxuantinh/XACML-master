3487e5382d31798ad1482fe8263da55f171a6715
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2021-04-30 19:22:41 +0700

    Initial commit

---
+++ XACML-PAP-ADMIN/src/main/java/com/att/research/xacml/admin/view/validators/Base64BinaryValidator.java
@@ -0,0 +1,46 @@
+/*
+ *                        AT&T - PROPRIETARY
+ *          THIS FILE CONTAINS PROPRIETARY INFORMATION OF
+ *        AT&T AND IS NOT TO BE DISCLOSED OR USED EXCEPT IN
+ *             ACCORDANCE WITH APPLICABLE AGREEMENTS.
+ *
+ *          Copyright (c) 2014 AT&T Knowledge Ventures
+ *              Unpublished and Not for Publication
+ *                     All Rights Reserved
+ */
+package com.att.research.xacml.admin.view.validators;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+
+import com.vaadin.data.Validator;
+
+public class Base64BinaryValidator implements Validator {
+	private static final long serialVersionUID = 1L;
+
+	public Base64BinaryValidator() {
+	}
+
+	@Override
+	public void validate(Object value) throws InvalidValueException {
+		if (value instanceof String) {
+			// try {
+			new Base64().decode((String) value);
+		// Base64().decode(String) inherited from 
+		// org.apache.commons.codec.binary.BaseNCodec
+		// does not throw DecoderException in version 1.10
+		// of org.apache.commons.codec (the version we are
+		// using).  This may need to be uncommented in later
+		// versions.
+		//
+		// TODO - Since this does not throw an exception under
+		//        any circumstance, I question whether it is
+		//        a valid method of validating the input value.
+		//
+		// } catch (DecoderException e) {
+		// 	throw new InvalidValueException(e.getLocalizedMessage());
+		// }
+		} else
+			throw new InvalidValueException("Unrecognized Base64 Binary");
+	}
+}
